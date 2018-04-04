a7f48be71d458e8a13a39d760d503c67f8892fa5
zhufeng <358244144@qq.com>
2018-04-04 11:04:51 -0700

    提交下项目了

---
+++ 网易新闻Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  网易新闻Demo
+//
+//  Created by bailing on 2018/4/4.
+//  Copyright © 2018年 zhufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 网易新闻Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  网易新闻Demo
+//
+//  Created by bailing on 2018/4/4.
+//  Copyright © 2018年 zhufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
